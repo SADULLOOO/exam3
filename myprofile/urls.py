9db4757b4fdd1a12682e3efb3f9dcddf1b839a9a
SA'DULLO <sadullo143@gmail.com>
2026-05-25 23:45:06 +0500

    tenth commit

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profile_view, name='profile'),
+    path('edit/', views.update_profile, name='profile_edit'),
 ]
--- myprofile/views.py
+++ myprofile/views.py
@@ -8,3 +8,24 @@ from .forms import ProfileForm
 def profile_view(request):
     profile, created = Profile.objects.get_or_create(user=request.user)
     return render(request, 'profiles/profile.html', {'profile': profile})
+
+
+@login_required
+def update_profile(request):
+    profile, created = Profile.objects.get_or_create(user=request.user)
+
+    form = ProfileForm(
+        request.POST or None,
+        request.FILES or None,
+        instance=profile
+    )
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+
+    return render(request, 'profiles/update_profile.html', {
+        'form': form,
+        'profile': profile
+    })
