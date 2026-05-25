d75332ea25d6fce2ca1ce805fc49f1c9e4888242
SA'DULLO <sadullo143@gmail.com>
2026-05-25 23:40:41 +0500

    nineth commit

--- myprofile/views.py
+++ myprofile/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+from .forms import ProfileForm
 
-# Create your views here.
+
+@login_required
+def profile_view(request):
+    profile, created = Profile.objects.get_or_create(user=request.user)
+    return render(request, 'profiles/profile.html', {'profile': profile})
