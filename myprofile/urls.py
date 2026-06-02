d4f69af5a0a047e3eee951624cfad02529b07681
SA'DULLO <sadullo143@gmail.com>
2026-06-03 01:57:03 +0500

    added new routes

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.profile_view, name='profile'),
     path('edit/', views.update_profile, name='profile_edit'),
+    path('request-license/', views.request_license_view, name='request_license'),
+    path('license-success/', views.license_success_view, name='license_success'),
+    path('boss-dashboard/', views.superuser_dashboard_view, name='superuser_dashboard'),
+    path('license-toggle/<int:license_id>/', views.toggle_license_status, name='toggle_license'),
 ]
