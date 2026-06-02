359d7714536730f3b10df3f00e28273c010643a3
SA'DULLO <sadullo143@gmail.com>
2026-06-03 00:15:44 +0500

    changed some logic for ordering

--- server/settings.py
+++ server/settings.py
@@ -130,11 +130,13 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 's95344349@gmail.com'
-EMAIL_HOST_PASSWORD = 'cqoumwtunhlvwtxa'
+EMAIL_HOST_PASSWORD = 'zntwtzrvvqaujczh' 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 AUTH_USER_MODEL = 'accounts.User'
