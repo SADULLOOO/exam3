9bfe3436292302efa59804e6cad6e4ee1ee9fa20
SA'DULLO <sadullo143@gmail.com>
2026-06-02 22:28:20 +0500

    changed some part of template car detail

--- cars/urls.py
+++ cars/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.home, name='home'),
     path('brand/<int:brand_id>/', views.brand_detail, name='brand_detail'),
