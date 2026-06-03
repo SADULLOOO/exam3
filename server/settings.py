76970145d3021f169382d9e8a01f5953ed667ab6
SA'DULLO <sadullo143@gmail.com>
2026-06-03 15:28:55 +0500

    changed settings

--- server/settings.py
+++ server/settings.py
@@ -130,8 +130,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
