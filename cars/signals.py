d0d5e2c41a44fb3381db7e09cdaa352cbf7cb397
SA'DULLO <sadullo143@gmail.com>
2026-06-03 15:41:06 +0500

    changed signals

--- cars/signals.py
+++ cars/signals.py
@@ -3,6 +3,15 @@ from django.dispatch import receiver
 from .models import Order, Credit
 
 @receiver(post_save, sender=Order)
-def order_status_changed(sender, instance, created, **kwargs):
-    if not created and instance.status == 'paid':
-        print(f"🔥 Signals works! Order №{instance.id} succesfull added!")
+def update_car_status_order(sender, instance, created, **kwargs):
+    if instance.status == 'paid':
+        car = instance.car
+        car.is_available = False
+        car.save()
+
+@receiver(post_save, sender=Credit)
+def update_car_status_credit(sender, instance, created, **kwargs):
+    if instance.status == 'approved':
+        car = instance.car
+        car.is_available = False
+        car.save()
